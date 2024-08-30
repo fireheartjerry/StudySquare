3107ec7e88300308c8cf1b2fd22d30c4224945dd
Jerry Li <fireheartjerry@gmail.com>
2024-08-30 18:46:39 -0400

    Delete unecessary files

--- src/db.py
+++ src/db.py
@@ -4,12 +4,8 @@ import os
 
 try:
     db = SQL("sqlite:///database.db")
-    problem_db = SQL("sqlite:///problemdatabase.db")
-except Exception as e:  # when testing
+except Exception as e:
     sys.stderr.write(str(e))
     if not os.path.exists("database.db"):
         open("database_test.db", "w").close()
-    if not os.path.exists("problemdatabase.db"):
-        open("problemdatabase.db","w").close()
-    problem_db = SQL("sqlite:///problemdatabase.db")
     db = SQL("sqlite:///database_test.db")
