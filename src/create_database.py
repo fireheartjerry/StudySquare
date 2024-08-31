169ec65e8136249ae548263c95479912339e6345
Jerry Li <fireheartjerry@gmail.com>
2024-08-31 08:17:29 -0400

    gitignore updates

--- src/create_database.py
+++ src/create_database.py
@@ -7,7 +7,15 @@ db.execute("""CREATE TABLE IF NOT EXISTS 'users' (
                 'username' varchar(20) NOT NULL UNIQUE,
                 'password' varchar(64) NOT NULL,
                 'email' varchar(128) UNIQUE,
-                'join_date' datetime NOT NULL DEFAULT(0)
-            );""")
+                'join_date' datetime NOT NULL DEFAULT(0),
+                'squares_created' integer NOT NULL DEFAULT(0),
+                'squares_joined' integer NOT NULL DEFAULT(0),
+                'total_seconds' integer NOT NULL DEFAULT(0),
+            );
+            CREATE TABLE IF NOT EXISTS 'squares' (
+                'id' varchar(64).
+                
+            );
+            """)
 
 db.execute("COMMIT")
